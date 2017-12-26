a75b398aab69aa76a044b118de8006f3d98bc9f5
Ivan Besarab <Ivan.Besarab@RockSoft.rocks>
2017-12-26 19:16:18 +0200

    Initial Commit

---
+++ G59L8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  G59L8
+//
+//  Created by Ivan Vasilevich on 12/26/17.
+//  Copyright © 2017 RockSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
